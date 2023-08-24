103f3d38eb8a8924eb1d7739c2144bcc11f9e361
mamun2001 <mamun.r2001@gmail.com>
2023-08-24 15:29:20 +0600

    upload with template

--- www/app/Views/upload_form.php
+++ www/app/Views/upload_form.php
@@ -1,11 +1,5 @@
-<head>
-    <title>Multitple File Upload</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    
-</head>
-
-<script type="text/javascript" src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+<?= $this->extend('layout/layout-a'); ?>
+<?= $this->section('content'); ?>
 
 <div class="container">
     <h2 class="text-center mt-5 mb-3">Upload Files</h2>
@@ -107,3 +101,5 @@
 		});
 	});
 </script>
+
+<?= $this->endSection(); ?>
