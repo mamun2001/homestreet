24310289010632351ac38f5ecdeae7e17374671e
mamun2001 <mamun.r2001@gmail.com>
2024-01-20 21:19:39 +0600

    admin dashboard

--- www/app/Controllers/Home.php
+++ www/app/Controllers/Home.php
@@ -2,18 +2,38 @@
 
 namespace App\Controllers;
 
-use App\Models\UsersModel;
+use App\Models\RequisitionModel;
+use App\Models\SummaryentryModel;
 
 class Home extends BaseController
 {
+    protected $requisitionModel;
+    protected $summaryentryModel;
+    protected $validation;
+
+    public function __construct()
+    {
+        $this->requisitionModel = new RequisitionModel();
+        $this->summaryentryModel = new SummaryentryModel();
+        $this->validation = \Config\Services::validation();
+    }
     public function index()
-    {        
+    {
         $userType = session()->get('user_type');
         if ($userType == 'Administrator') {
-            return view('dashboard/admin');
+            $data['pending'] = array();
+            $dat = $this->requisitionModel->where('status', 'Pending')->findAll();
+            $data['pending'] = count($dat);
+
+            $date = date_create(date("Y-m-d"));
+            date_sub($date, date_interval_create_from_date_string("7 days"));
+            $dt = date_format($date, "Y-m-d");
+            $sum = $this->summaryentryModel->where('date(date_time)>=', $dt)->findAll();
+            $data['vouchers'] = count($sum);
+
+            return view('dashboard/admin', $data);
         } else {
             return view('dashboard/home');
         }
-
     }
 }
--- www/app/Views/dashboard/admin.php
+++ www/app/Views/dashboard/admin.php
@@ -4,8 +4,8 @@
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
 <link rel="stylesheet" href="https://cdn.datatables.net/1.10.22/css/jquery.dataTables.min.css" />
 
-<div class="container">
-    <table class="table table-striped table-bordered" id="user-table">
+<div class="container-fluid">
+    <!-- <table class="table table-striped table-bordered" id="user-table">
         <thead>
             <tr>
                 <th>No</th>
@@ -14,21 +14,82 @@
             </tr>
         </thead>
         <tbody></tbody>
-    </table>
+    </table> -->
+    <div class="row">
+        <div class="col-lg-3 col-6">
+            <div class="small-box bg-info">
+                <div class="inner">
+                    <h3>
+                        <?= $pending ?>
+                    </h3>
+                    <p>Pending Requisitions</p>
+                </div>
+                <div class="icon">
+                    <i class="ion ion-bag"></i>
+                </div>
+                <a href="<?= route_to('requisition/admin'); ?>" class="small-box-footer">More info <i
+                        class="fas fa-arrow-circle-right"></i></a>
+            </div>
+        </div>
+        <!-- ./col -->
+        <div class="col-lg-3 col-6">
+            <div class="small-box bg-success">
+                <div class="inner">
+                    <!-- <h3>53<sup style="font-size: 20px">%</sup></h3> -->
+                    <h3>
+                        <?= $vouchers ?>
+                    </h3>
+                    <p>Vouchers Last 7 Days</p>
+                </div>
+                <div class="icon">
+                    <i class="ion ion-stats-bars"></i>
+                </div>
+                <a href="<?= route_to('voucher/admin'); ?>" class="small-box-footer">More info <i
+                        class="fas fa-arrow-circle-right"></i></a>
+            </div>
+        </div>
+        <!-- ./col -->
+        <!-- <div class="col-lg-3 col-6">
+            <div class="small-box bg-warning">
+                <div class="inner">
+                    <h3>44</h3>
+                    <p>User Registrations</p>
+                </div>
+                <div class="icon">
+                    <i class="ion ion-person-add"></i>
+                </div>
+                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+        </div> -->
+        <!-- ./col -->
+        <!-- <div class="col-lg-3 col-6">
+            <div class="small-box bg-danger">
+                <div class="inner">
+                    <h3>65</h3>
+                    <p>Unique Visitors</p>
+                </div>
+                <div class="icon">
+                    <i class="ion ion-pie-graph"></i>
+                </div>
+                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+        </div> -->
+        <!-- ./col -->
+    </div>
 </div>
 
 <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
 <script src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js"></script>
 
 <script>
-    $(document).ready(function () {
-        $('#user-table').DataTable({
-            processing: true,
-            serverSide: true,
-            ajax: 'user/ajaxUser',
-            columnDefs: []
-        });
-    });
+    // $(document).ready(function () {
+    //     $('#user-table').DataTable({
+    //         processing: true,
+    //         serverSide: true,
+    //         ajax: 'user/ajaxUser',
+    //         columnDefs: []
+    //     });
+    // });
 </script>
 
 <?= $this->endSection(); ?>
